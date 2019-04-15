05b8a193934f4c908488858f6c165921402bcace
donat <donat@TeamCity>
2019-04-15 15:34:21 +0000

    TeamCity change in 'Tooling / Buildship / Promotion' project: 'Promote Snapshot' build configuration settings were updated

--- .teamcity/patches/buildTypes/Promote30_Snapshot.kts
+++ .teamcity/patches/buildTypes/Promote30_Snapshot.kts
@@ -1,8 +1,6 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.GradleBuildStep
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -11,119 +9,6 @@ To apply the patch, change the buildType with id = 'Promote30_Snapshot'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Promote30_Snapshot")) {
-    expectSteps {
-        gradle {
-            name = "Build and upload update site for Eclipse 4.3 (Kepler)"
-            id = "RUNNER_1"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=43 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 4.4 (Luna)"
-            id = "RUNNER_2"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=44 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 4.5 (Mars)"
-            id = "RUNNER_3"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=45 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 4.6 (Neon)"
-            id = "RUNNER_4"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=46 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 4.7 (Oxygen)"
-            id = "RUNNER_5"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=47 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 4.8 (Photon)"
-            id = "RUNNER_6"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=48 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 2018-09"
-            id = "RUNNER_7"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=49 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 2018-12"
-            id = "RUNNER_8"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=410 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-        }
-        gradle {
-            name = "Build and upload update site for Eclipse 2019-03"
-            id = "RUNNER_9"
-            tasks = "clean build uploadUpdateSite"
-            buildFile = ""
-            gradleParams = """
-                --exclude-task eclipseTest
-                -Peclipse.version=411 -Pcompiler.location='%linux.java8.oracle.64bit%/bin/javac' -Pbuild.invoker=%build.invoker% -Prelease.type=%eclipse.release.type% -PECLIPSE_ORG_FTP_HOST=build.eclipse.org -PECLIPSE_ORG_FTP_USER=%eclipse.downloadServer.username% -PECLIPSE_ORG_FTP_PASSWORD=%eclipse.downloadServer.password% -PECLIPSE_ORG_FTP_UPDATE_SITES_PATH=/home/data/httpd/download.eclipse.org/buildship/updates -PECLIPSE_ORG_TEMP_PATH=/home/data/httpd/download.eclipse.org/buildship/temp -PECLIPSE_ORG_MIRROR_PATH=/buildship/updates
-                --stacktrace -Declipse.p2.mirror=false
-            """.trimIndent()
-        }
-    }
-    steps {
-        update<GradleBuildStep>(0) {
-            id = "RUNNER_1"
-            enabled = false
-        }
-    }
-
     requirements {
         add {
             matches("teamcity.agent.name", "dev(3|7)-agent1", "TEMPLATE_RQ_2")
